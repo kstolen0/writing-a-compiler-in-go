87678a654359e531582b56aa973d041be0f89ab4
Kristian Stolen <kstolen0@gmail.com>
2025-06-07 20:47:36 +0800

    support index operations

--- code/src/monkey/code/code.go
+++ code/src/monkey/code/code.go
@@ -36,6 +36,7 @@ const (
 
 	OpArray
 	OpHash
+	OpIndex
 
 	OpNull
 )
@@ -60,6 +61,7 @@ var definitions = map[Opcode]*Definition{
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpIndex", []int{}},
 	OpNull:          {"OpNull", []int{}},
 }
 
